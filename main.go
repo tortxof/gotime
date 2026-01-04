57d9d152e41e4764d539014efd9247e00387b4d6
Daniel Jones <tortxof@gmail.com>
2026-01-04 16:49:35 -0500

    Add cors allow methods header

--- main.go
+++ main.go
@@ -117,6 +117,7 @@ func timeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET,OPTIONS")
 	w.Header().Set("Cache-Control", "no-store")
 	w.Write(buf)
 }
